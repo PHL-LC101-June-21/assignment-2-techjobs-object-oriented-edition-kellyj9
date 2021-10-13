60952b1c5f9997617cce6d29b72a8c2617f3f068
Kelly Jones <kellymj6@gmail.com>
2021-10-12 23:00:32 -0400

    Used TDD to Build the toString Method

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -21,7 +21,8 @@ public class Job {
         nextId++;
     }
 
-    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+    public Job(String name, Employer employer, Location location,
+               PositionType positionType, CoreCompetency coreCompetency) {
         this();
         this.name = name;
         this.employer = employer;
@@ -69,8 +70,8 @@ public class Job {
         return coreCompetency;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public void setName(String name) {
@@ -92,4 +93,95 @@ public class Job {
     public void setCoreCompetency(CoreCompetency coreCompetency) {
         this.coreCompetency = coreCompetency;
     }
+
+    /*
+    When passed a Job object, this method returns a string that contains a
+    blank line before and after the job information.
+    The string should contain a label for each field, followed by the data stored in
+    that field. Each field should be on its own line.
+     If a field is empty, the method should add, “Data not available” after the label.
+    (Bonus) If a Job object ONLY contains data for the id field, the method should
+    return, “OOPS! This job does not seem to exist.”
+      */
+    @Override
+    public String toString() {
+        String strDataNotAvailable = "Data not available";  // to denote an empty field
+
+        // string that gets returned when all fields are empty or null EXCEPT for the id
+        String strDataDoesNotExist = "\nOOPS! This job does not seem to exist.\n";
+
+        String strEmployerValue = employer.getValue();
+        String strLocationValue = location.getValue();
+        String strPositionTypeValue = positionType.getValue();
+        String strCoreCompetencyValue = coreCompetency.getValue();
+
+        // if all fields are empty or null (EXCEPT for id)
+        // then return a message that the job doesn't seem to exist...
+        if ((name == null || name.equals("")) &&
+                (strEmployerValue == null || strEmployerValue.equals("")) &&
+                (strLocationValue == null || strLocationValue.equals("")) &&
+                (strPositionTypeValue == null || strPositionTypeValue.equals("")) &&
+                (strCoreCompetencyValue == null || strCoreCompetencyValue.equals(""))){
+            return strDataDoesNotExist;
+        }
+        // ...otherwise concatenate the field headings and field values to create a string
+        else {
+
+            // start creating the string to return
+            String tempToString = "\nID: " + id;
+
+            // append the field heading
+            tempToString += "\nName: ";
+            //  if the field value is null or empty, denote that the
+            //  data is not available for that field. Otherwise, append the field value.
+            if (name == null || name.equals("")) {
+                tempToString += strDataNotAvailable;
+            } else {
+                tempToString += name;
+            }
+
+            // append the field heading
+            tempToString += "\nEmployer: ";
+            //  if the field value is null or empty, denote that the
+            //  data is not available for that field. Otherwise, append the field value.
+            if (strEmployerValue == null || strEmployerValue.equals("")) {
+                tempToString += strDataNotAvailable;
+            } else {
+                tempToString += strEmployerValue;
+            }
+
+            // append the field heading
+            tempToString += "\nLocation: ";
+            //  if the field value is null or empty, denote that the
+            //  data is not available for that field. Otherwise, append the field value.
+            if (strLocationValue == null || strLocationValue.equals("")) {
+                tempToString += strDataNotAvailable;
+            } else {
+                tempToString += strLocationValue;
+            }
+
+            // append the field heading
+            tempToString += "\nPosition Type: ";
+            //  if the field value is null or empty, denote that the
+            //  data is not available for that field. Otherwise, append the field value.
+            if (strPositionTypeValue == null || strPositionTypeValue.equals("")) {
+                tempToString += strDataNotAvailable;
+            } else {
+                tempToString += strPositionTypeValue;
+            }
+
+            // append the field heading
+            tempToString += "\nCore Competency: ";
+            //  if the field value is null or empty, denote that the
+            //  data is not available for that field. Otherwise, append the field value.
+            if (strCoreCompetencyValue == null || strCoreCompetencyValue.equals("")) {
+                tempToString += strDataNotAvailable;
+            } else {
+                tempToString += strCoreCompetencyValue;
+            }
+
+            return tempToString += "\n";
+        }
+    }
+
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -13,15 +13,21 @@ import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+  // test the empty constructor
   @Test
   public void testSettingJobId() {
-      Job job1 = new Job();
-      Job job2 = new Job();
-      assertNotEquals(job1, job2);
+    // create two Job objects using the empty constructor
+    Job job1 = new Job();
+    Job job2 = new Job();
+
+    // use assertNotEquals to verify that the IDs of the two objects are distinct
+    assertNotEquals(job1, job2);
   }
 
+  // test the full constructor
   @Test
     public void testJobConstructorSetsAllFields() {
+    // declare and initialize a new Job object
       Job job3 = new Job(
               "Product tester",
               new Employer("ACME"),
@@ -44,10 +50,114 @@ public class JobTest {
       assertEquals("Persistence", job3.getCoreCompetency().getValue());
     }
 
+    // Test the equals method.
+    // Two Job objects are considered equal if they have the same id value,
+    // even if one or more of the other fields differ.
+    // Similarly, the two objects are NOT equal
+    // if their id values differ, even if all the other fields are identical.
     @Test
     public  void testJobsForEquality(){
-      Job job4 = new Job("QA analyst", new Employer("Vanguard"), new Location("Philadelphia"), new PositionType("Quality control"), new CoreCompetency("Unit testing"));
-      Job job5 = new Job("QA analyst", new Employer("Vanguard"), new Location("Philadelphia"), new PositionType("Quality control"), new CoreCompetency("Unit testing"));
+    // Generate two Job objects that have identical field values (EXCEPT for id)
+      Job job4 = new Job(
+              "QA analyst",
+              new Employer("Vanguard"),
+              new Location("Philadelphia"),
+              new PositionType("Quality control"),
+              new CoreCompetency("Unit testing")
+      );
+      Job job5 = new Job(
+              "QA analyst",
+              new Employer("Vanguard"),
+              new Location("Philadelphia"),
+              new PositionType("Quality control"),
+              new CoreCompetency("Unit testing")
+      );
+      // test that equals returns false to make sure the objects have different id's
       assertFalse(job4.equals(job5));
   }
+
+  // When passed a Job object, toString() should return a string that contains
+  // a blank line before and after the job information.
+  @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+      // create a Job object
+      Job job6 = new Job(
+              "Junior Web developer",
+              new Employer("Catalyst"),
+              new Location("Baltimore"),
+              new PositionType("Dev"),
+              new CoreCompetency("HTML")
+      );
+      // When passed a Job object, it should return a string that contains a blank line
+      // before and after the job information.
+      assertEquals('\n',
+              job6.toString().charAt(0));
+      assertEquals('\n',
+              job6.toString().charAt(job6.toString().length()-1));
+  }
+
+  /// When passed a Job object, the toString() should return a string
+  // that contains a label for each field, followed by the data stored
+  // in that field. Each field should be on its own line.
+  @Test
+  public void testToStringContainsCorrectLabelsAndData(){
+    // create a Job object
+    Job job7 = new Job(
+            "Web developer",
+            new Employer("Catalyst"),
+            new Location("Baltimore"),
+            new PositionType("Dev"),
+            new CoreCompetency("JavaScript")
+    );
+
+    assertEquals("\nID: " + job7.getId() +
+                    "\nName: Web developer" +
+                    "\nEmployer: Catalyst" +
+                    "\nLocation: Baltimore" +
+                    "\nPosition Type: Dev" +
+                    "\nCore Competency: JavaScript" +
+                    "\n", job7.toString());
+  }
+
+  // If a field is empty, the method should add,
+  // “Data not available” after the label.
+  @Test
+  public void testToStringHandlesEmptyField() {
+    // create a Job object with some fields empty
+    Job job8 = new Job(
+            "Web developer II",
+            new Employer("Catalyst"),
+            new Location(""),
+            new PositionType("Dev"),
+            new CoreCompetency("")
+    );
+
+    String strDataNotAvailable = "Data not available";
+    assertEquals(
+            "\nID: " + job8.getId() +
+            "\nName: Web developer II" +
+            "\nEmployer: Catalyst" +
+            "\nLocation: " + strDataNotAvailable +
+            "\nPosition Type: Dev" +
+            "\nCore Competency: " + strDataNotAvailable +
+            "\n",
+            job8.toString());
+
+  }
+
+  // (Bonus) If a Job object ONLY contains data for the id field, the method
+  // should return, “\nOOPS! This job does not seem to exist.\n”
+  @Test
+  public void testToStringHandlesJobObjectOnlyContainsDataForID() {
+    // create a Job object with all empty fields (note: the job id will still be generated)
+    Job job9 = new Job("",
+            new Employer(""),
+            new Location(""),
+            new PositionType(""),
+            new CoreCompetency("")
+    );
+    String strDataDoesNotExist = "\nOOPS! This job does not seem to exist.\n";
+    assertEquals(strDataDoesNotExist, job9.toString());
+  }
+
 }
